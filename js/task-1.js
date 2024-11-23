3d73a627005a8e77b502c4d266ba7cc94ab391cc
Viktor Datii <datiyvk@gmail.com>
2024-11-23 22:40:41 +0200

    js-hw4

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,18 @@
-function slugify(title = "") {
-  return title.toLowerCase().split(" ").join("-");
-}
+const isEnoughCapacity = function (products, containerSize) {
+  let productsQuantity = 0;
 
-console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+  const keys = Object.keys(products);
+  for (const key of keys) {
+    productsQuantity += products[key];
+  }
+
+  return productsQuantity <= containerSize;
+};
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
